4719bce587123a4b0d24f00ddec4975c20d860b0
Alef <Alef@DESKTOP-ALEF>
2024-09-11 19:39:32 -0300

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args);
+        {
+
+
+        //const double Pi = 3.141159;
+        //double radius = 5.3;
+        //double area = Pi * (radius * radius);
+
+        //Console.WriteLine("O valor da área é: " + area);
+        //Console.ReadLine();
+        //String[] nomes = new String[] {"Alef Rodrigues", "Yasmim Queiroz"};
+
+        //Console.WriteLine("O valor da posição 0 é:" + nomes[1]);
+        //Console.ReadLine();
+
+        //List<string> nomes = new List<string>();
+        //nomes.Add("Alef");
+        //nomes.Add("Yasmim");
+        //nomes.Add("Suzana");
+
+
+        //Console.WriteLine("Posição inicial da lista: " + nomes[0] + ", com o tamanho de lista: " + nomes.Count());
+        //Console.ReadLine();
+
+        //bool existeNaLista = nomes.Contains("Yasmim");
+        //Console.WriteLine("Existe na lista? " + existeNaLista);
+        //Console.ReadLine();
